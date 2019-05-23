2f4093522ee1e52e5e4b098322cb3a6ca1ccb7f8
Raúl Angulo Martínez <raul@D55-MACAIR-006.local>
2019-05-23 17:51:10 +0200

    Añadido prosgrl database

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,10 @@
 
 ActiveRecord::Schema.define(version: 2019_05_15_174906) do
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "name"
     t.string "password_digest"
