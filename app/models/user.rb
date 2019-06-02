dde1436c0970de163e7450d01f2276268d1864f3
Daniel Molist <daniel.molist@gmail.com>
2019-06-02 20:42:37 +0200

    added UserResources table and relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
     validates :password, presence: true
+    has_many :user_resources, dependent: :destroy
+    has_many :resources, through: :user_resources
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_26_110304) do
+ActiveRecord::Schema.define(version: 2019_06_02_181855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,21 @@ ActiveRecord::Schema.define(version: 2019_05_26_110304) do
     t.string "lastname"
     t.integer "age"
     t.integer "gender"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "resources", force: :cascade do |t|
+    t.string "name"
+    t.string "localization"
+    t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_resources", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "resource_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
