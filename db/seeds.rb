8ce947c9f0819f8157212861a55f4a48a5d41178
Daniel Molist <daniel.molist@gmail.com>
2019-06-14 11:15:19 +0200

    fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: "Admin", email: "user@example.com", password: "changeme", admin: true, active: true)
+
 5.times do
     User.create(name: Faker::Name.name, email: Faker::Internet.email, password: "messi")
 end
-    User.create(name: "Admin", email: "user@example.com", password: "changeme", admin: true, active: true)
+    
