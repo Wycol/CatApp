4084fe6bbcf0052116811274d9f706fd2e518308
Raúl Angulo Martínez <raul@D55-MACAIR-006.local>
2019-06-14 19:47:41 +0200

    Añadidos vistas json para api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,13 @@
 module Api::V1
     class UsersController < ApiController
+        before_action :set_user, only: [:show, :edit, :destroy, :update]
 
         def index
             @users = User.all
         end
 
+        def show
+        end
         def create
             @user = User.new(user_params)
             
@@ -19,6 +22,10 @@ module Api::V1
 
         def user_params
             params.permit(:name, :email, :password, :password_confirmation)
+        end
+
+        def set_user
+            @user = User.find(params[:id])
         end
 
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_10_131444) do
+ActiveRecord::Schema.define(version: 2019_06_14_114434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2019_06_10_131444) do
     t.integer "gender"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "description"
+    t.text "description"
     t.float "height"
     t.float "weight"
     t.integer "eye"
