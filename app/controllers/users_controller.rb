f912a14b1280ece7e87fe18204398bac1f4a80aa
Daniel Molist <daniel.molist@gmail.com>
2019-06-14 11:15:32 +0200

    cambios en front

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
     before_action :set_user, only: [:show, :edit, :destroy, :update, :activate, :deactivate]
     before_action :require_admin, except: [:new, :create]
     def index
-        @users = User.all.where(admin: false).order(:name)
+        @users = User.all.order(:id)
     end
 
     def new
