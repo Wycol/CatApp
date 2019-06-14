6f711c48cff25ddce0f749603f817f567f419e6c
Raúl Angulo Martínez <raul@D55-MACAIR-006.local>
2019-06-14 10:46:27 +0200

    Avellana

--- app/models/missing_person.rb
+++ app/models/missing_person.rb
@@ -1,6 +1,6 @@
 class MissingPerson < ApplicationRecord
     enum gender: [:Masculino, :Femenino]
-    enum eye: [:Marrón, :Ambar, :Abellana, :Verde, :Azul, :Gris]
+    enum eye: [:Marrón, :Ambar, :Avellana, :Verde, :Azul, :Gris]
     enum race: [:Blanco, :Negro, :Mulato, :Chino, :Indio, :Arabe]
     enum hair: [:Corto, :Largo, :Calvo]
     enum hair_color: [:Castaño, :Moreno, :Rubio, :Pelirrojo, :Canoso]
